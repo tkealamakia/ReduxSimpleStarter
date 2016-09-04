772abd639fb9f617983480e6fa692cfff7b4fa0d
tkealamakia <tkealamakia@gmail.com>
2016-09-04 07:46:33 -0600

    Add video example page

--- src/components/search_bar.js
+++ src/components/search_bar.js
@@ -1,10 +1,26 @@
 import React, { Component } from 'react';
 
 class SearchBar extends Component {
-  render() {
-    return <input />;
+  constructor(props) {
+    super(props);
+    this.state = { term:'' };
   }
 
+  render() {
+    return (
+      <div className="search-bar">
+        <input
+          value={this.state.term}
+          onChange={event => this.onInputChange(event.target.value)} />
+      </div>
+    );
+  }
+
+  onInputChange(term) {
+    this.setState({term});
+    this.props.onSearchTermChange(term);
+
+  }
 }
 
 export default SearchBar;
