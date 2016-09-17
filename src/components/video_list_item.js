9194673097ca5718fd116c8f4333bcb72effa8a4
tkealamakia <tkealamakia@gmail.com>
2016-09-17 03:23:00 -0600

    Adding rallycoding linter

--- src/components/video_list_item.js
+++ src/components/video_list_item.js
@@ -1,13 +1,13 @@
 import React from 'react';
 
-const VideoListItem = ({video, onVideoSelect}) => {
+const VideoListItem = ({ video, onVideoSelect }) => {
   const imageUrl = video.snippet.thumbnails.default.url;
 
   return (
     <li onClick={() => onVideoSelect(video)} className="list-group-item">
       <div className="video-list media">
         <div className="media-left">
-          <img className="media-object" src={imageUrl} />
+          <img alt="" className="media-object" src={imageUrl} />
         </div>
       </div>
       <div className="media-body">
